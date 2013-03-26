466854ad2de4e6fbea4867accc95ff2bfbee3e4a
Manny Fleurmond <funkatronic@gmail.com>
2013-03-25 22:58:21 -0500

    Code to help with auto loading in php.
    `camel_to_dash` takes a string that is in camel hump format, makes it
    all lowerwer case and adds dashes in between the words.
    `underscore_to_slash` takes underscores and converts them to slashes.
    Both can be used to convert strings like class names into workable URLs

---
+++ autoload_test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php 
+function camel_to_dash( $className) {
+	return 	strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $className)); 
+}
+
+function underscore_to_slash( $className ) {
+	return preg_replace('#\\\|_(?!.+\\\)#','/',$className );
+}
+echo underscore_to_slash( camel_to_dash('Admin_WinOfEpicness') );
+?>
+</body>
+</html>
